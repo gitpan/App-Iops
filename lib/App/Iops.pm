665e888bc9a64aa24bc8fbee4e0b3237d8a02aaa
Joshua ben Jore <jjore@cpan.org>
2011-06-16 12:40:11 +0000

    Import of JJORE/App-Iops-0.002 from CPAN.
    gitpan-cpan-distribution: App-Iops
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         JJORE/App-Iops-0.002.tar.gz
    gitpan-cpan-author:       JJORE
    gitpan-cpan-maturity:     released

--- lib/App/Iops.pm
+++ lib/App/Iops.pm
@@ -1,31 +1,51 @@
 package App::Iops;
 BEGIN {
-  $App::Iops::VERSION = '0.001';
+  $App::Iops::VERSION = '0.002';
 }
 
 =head1 NAME
 
-iops - Show process I/O operations
+App::Iops - Show process I/O operations
 
 =head1 VERSION
 
-version 0.001
+version 0.002
 
 =head1 SYNOPSIS
 
-  iops <pid>
+  iops [options]
 
+      --pid [pid]
       --help
 
 =head1 DESCRIPTION
 
 Summarize a process's I/O operations in real time.
 
+Attach to an existing process:
+
+  $ iops -p 3251
+  read /dev/random...............................
+  write /var/log/message..
+  close /dev/random
+
+=head1 API
+
+Methods are still subject to change.
+
+=over
+
+=item new
+
+=item run
+
+=back
+
 =cut
 
 use strict;
 
-use English -no_match_vars;
+use English '-no_match_vars';
 use Getopt::Long ();
 use Pod::Usage ();
 
@@ -45,43 +65,45 @@ sub new {
 sub run {
     my $self = shift;
 
-    $self->read_arguments( @_ );
+    $self->_read_arguments( @_ );
 
-    $self->proc_readlinks;
-    $self->open_strace;
-    $self->watch_iops;
+    $self->_proc_readlinks;
+    $self->_open_strace_pid;
+    $self->_watch_iops;
 
     # NEVER REACHED
+    return;
 }
 
-sub watch_iops {
+sub _watch_iops {
     my ($self) = @_;
 
     $OUTPUT_AUTOFLUSH = -t STDOUT;
 
+    local $/ = "\n";
     while ( my $iop = readline $self->{strace_fh} ) {
         chomp $iop;
 
         my ( $op, $fd, $fn );
         if ( ( $fd ) = $iop =~ /^close\(([0-9]+)/ ) {
             $self->{files}{$fd} ||= readlink( "/proc/$self->{pid}/fd/$fd" );
-            $self->iop( 'close ' . ( defined $self->{files}{$fd} ? $self->{files}{$fd} : $fd ) );
+            $self->_iop( 'close ' . ( defined $self->{files}{$fd} ? $self->{files}{$fd} : $fd ) );
             delete $self->{files}{$fd};
         }
         elsif ( ( $op, $fd ) = $iop =~ /^(\w+)\(([0-9]+)/ ) {
-            my $fn = $self->{files}{$fd} ||= readlink( "/proc/$self->{pid}/fd/$fd" );
+            $fn = $self->{files}{$fd} ||= readlink( "/proc/$self->{pid}/fd/$fd" );
             my $color = $op eq 'read' ? "\e[33m" : "\e[31m";
-            $self->iop( "$color$op\e[0m " . ( defined $fn ? $fn : $fd ) );
+            $self->_iop( "$color$op\e[0m " . ( defined $fn ? $fn : $fd ) );
         }
         elsif ( ( $op, $fn ) = $iop =~ /^(\w+)\("([^"]+)/ ) {
-            $self->iop( "$op $fn" );
+            $self->_iop( "$op $fn" );
         }
     }
 
     return;
 }
 
-sub read_arguments {
+sub _read_arguments {
     my $self = shift;
 
     local @ARGV = @_;
@@ -93,25 +115,27 @@ sub read_arguments {
                 -verbose => 2,
             );
         },
+        'pid=i',
     )
-      or Pod::Usage::pod2usage( -exitval => 2 );
-
-    my $pid;
-    if ( ! $self->{pid} && @ARGV == 1 && $ARGV[0] =~ /^[0-9]+\z/ ) {
-        $self->{pid} = $ARGV[0];
-    }
-    else {
-        Pod::Usage::pod2usage( -exitval => 2 );
+      or Pod::Usage::pod2usage(
+          -exitval => 2,
+          -verbose => 2,
+      );
+    if (@ARGV) {
+        Pod::Usage::pod2usage(
+            -exitval => 2,
+            -verbose => 2,
+        );
     }
 
     return;
 }
 
-sub proc_readlinks {
+sub _proc_readlinks {
     my ($self) = @_;
 
     opendir FD, "/proc/$self->{pid}/fd"
-      or die "Can't open /proc/$self->{pid}/fd: $!";
+      or die "Can't open /proc/$self->{pid}/fd: $ERRNO";
     my %files;
     for ( readdir FD ) {
         next if $_ eq '.' || $_ eq '..';
@@ -129,7 +153,7 @@ sub proc_readlinks {
     return;
 }
 
-sub open_strace {
+sub _open_strace_pid {
     my ($self) = @_;
 
     my @strace_cmd = (
@@ -142,14 +166,16 @@ sub open_strace {
     );
     my $strace_pid = open my ($strace_fh), '-|', @strace_cmd;
     if ( ! $strace_pid ) {
-        die "Can't [@strace_cmd]: $!";
+        die "Can't [@strace_cmd]: $ERRNO";
     }
 
     $self->{strace_fh}  = $strace_fh;
     $self->{strace_pid} = $strace_pid;
+
+    return;
 }
 
-sub iop {
+sub _iop {
     my ($self, $text) = @_;
 
     if ( $text eq $self->{prev} ) {
@@ -162,8 +188,8 @@ sub iop {
     return;
 }
 
-"We make an average of 257 cups a day on Tuesdays but volume is
+'We make an average of 257 cups a day on Tuesdays but volume is
 trending downwards because with the onset of of warmer weather
 customers begin ordering more cold, non-coffee based drinks such as
 iced tea. The espresso to drip ratio is approximately 1.67 to 1
-suggesting that ...";
+suggesting that ...';
